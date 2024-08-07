8fd753eb5f4fd27c0e287d60d508998bbbde148c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-08-08 03:53:34 +0530

    add polls view

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(_: HttpRequest):
+    return HttpResponse("Hello, world. 7ca7066d is the polls index.")
