e1c02838632ce65c32c217a298993bb2392454ac
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-08-10 21:57:13 +0530

    add template for detail.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,23 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
+from polls.models import Question
 
 
 # Create your views here.
-def index(_: HttpRequest):
+def index(request: HttpRequest):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    ctx = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", ctx)
 
 
-def detail(_: HttpResponse, question_id: int):
-    return HttpResponse(f"You're looking at question {question_id}")
+def detail(request: HttpResponse, question_id: int):
+    try:
+        q = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404(f"Question {question_id} does not exist.")
+
+    ctx = {"question": q}
+    return render(request, "polls/detail.html", ctx)
 
 
 def results(_: HttpResponse, question_id):
