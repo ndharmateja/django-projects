72ed56d1161207dc00d4f612a6012adb011d3d88
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-08-12 18:24:32 +0530

    fix pk in urls for generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,8 +4,8 @@ from polls import views
 app_name = "polls"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("<int:question_id>/", views.DetailView.as_view(), name="detail"),
-    path("<int:question_id>/results", views.ResultsView.as_view(), name="results"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote", views.vote, name="vote"),
     path("owner", views.owner, name="owner"),
 ]
