db725f1bcecf6cf612d82260cfd9f6c27b7bb8f3
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-03-02 11:38:43 +0530

    serve static files

--- mysite2/mysite2/urls.py
+++ mysite2/mysite2/urls.py
@@ -15,10 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+import os
+
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
+
+# Up two folders to serve "site" content
+# BASE_DIR will be "/.../mysite2"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# SITE_ROOT will be "/.../mysite2/site"
+SITE_ROOT = os.path.join(BASE_DIR, "site")
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+    re_path(
+        r"^site/(?P<path>.*)$",
+        serve,
+        {"document_root": SITE_ROOT, "show_indexes": True},
+        name="site_path",
+    ),
 ]
