37be5ad0ea86d062b7f27656017b9da35561dfa1
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-03-03 16:13:37 +0530

    add string representations of the models

--- mysite2/polls/models.py
+++ mysite2/polls/models.py
@@ -14,7 +14,14 @@ class Question(Model):
     pub_date = DateTimeField("date published")
 
 
+    def __str__(self):
+        return self.question_text
+
+
 class Choice(Model):
     question = ForeignKey(Question, on_delete=CASCADE)
     choice_text = CharField(max_length=200)
     votes = IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
