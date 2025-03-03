54300d960b84313030e84f94a1133f299adeabef
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-03-03 15:40:18 +0530

    add models Question, Choice

--- mysite2/polls/models.py
+++ mysite2/polls/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from django.db.models import (
+    CASCADE,
+    CharField,
+    DateTimeField,
+    ForeignKey,
+    IntegerField,
+    Model,
+)
+
 
 # Create your models here.
+class Question(Model):
+    question_text = CharField(max_length=200)
+    pub_date = DateTimeField("date published")
+
+
+class Choice(Model):
+    question = ForeignKey(Question, on_delete=CASCADE)
+    choice_text = CharField(max_length=200)
+    votes = IntegerField(default=0)
