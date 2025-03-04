5a2bc064982958973c7de5493853966f49ec9765
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-03-04 17:43:57 +0530

    add placeholders for detail, results & vote views

--- mysite2/polls/views.py
+++ mysite2/polls/views.py
@@ -4,4 +4,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. 6e55b7ae is the polls index.")
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
