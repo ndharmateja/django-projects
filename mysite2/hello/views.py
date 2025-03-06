68462b3094e7002e0a3609edab2c1e5e35c55f27
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-03-06 09:08:16 +0530

    Add views for the hello app

--- mysite2/hello/views.py
+++ mysite2/hello/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def myview(request: HttpRequest) -> HttpResponse:
+    num_visits = request.session.get("num_visits", 0) + 1
+    request.session["num_visits"] = num_visits
+    if num_visits > 4:
+        del request.session["num_visits"]
+    resp = HttpResponse(f"View count: {num_visits}")
+    resp.set_cookie("dj4e_cookie", "eea1c0b5", max_age=1000)
+    return resp
