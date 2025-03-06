dae7401ab1bab51d8c717303a65aed695289acb2
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-03-06 08:59:29 +0530

    setup home app in the project

--- mysite2/mysite2/urls.py
+++ mysite2/mysite2/urls.py
@@ -19,6 +19,7 @@ import os
 
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 from django.views.static import serve
 
 # Up two folders to serve "site" content
@@ -28,6 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SITE_ROOT = os.path.join(BASE_DIR, "site")
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="home/main.html")),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
     re_path(
